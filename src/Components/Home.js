632b7a3a0cd7af61c5569e10cb5579ea3388f3f6
Yzer Jake Navarroza <yzerjakenavarroza@gmail.com>
2024-10-17 08:45:23 +0800

    Added custom usernames

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,26 +6,26 @@ const Home = () => {
     <div className="text-center mt-5">
       <h1><b>˗ˏˋ Welcome to Yzer's Profile ★ ˎˊ˗</b></h1>
       <p>This is my personal space where I share details about myself, my skills, and how to get in touch.</p>
-      <p>Navigate on my page using the links above. I hope you have an amazing day! ⋆｡°✩</p>
+      <p>Navigate on my page using the links above, or connect with me using my social media accounts below. I hope you have an amazing day! ⋆｡°✩</p>
 
       <div className="social-media mt-4">
-        <h3>Connect with me:</h3>
+        <h3><b>Connect ˚ʚ♡ɞ˚</b></h3>
         <ul style={{ listStyleType: 'none', padding: 0 }}>
           <li style={{ margin: '10px' }}>
             <FaFacebookF style={{ color: 'black', marginRight: '8px' }} />
-            <span>facebook_username</span>
+            <a href="https://www.facebook.com/yzerjake"><span>Yzer Jake Navarroza</span></a>
           </li>
           <li style={{ margin: '10px' }}>
             <FaInstagram style={{ color: 'black', marginRight: '8px' }} />
-            <span>instagram_username</span>
+            <a href="https://www.instagram.com/wxyzerthanyu"><span>@wxyzerthanyu</span></a>
           </li>
           <li style={{ margin: '10px' }}>
             <FaTwitter style={{ color: 'black', marginRight: '8px' }} />
-            <span>twitter_username</span>
+            <a href="https://x.com/wxyzerthanyu"><span>@wxyzerthanyu</span></a>
           </li>
           <li style={{ margin: '10px' }}>
             <FaDiscord style={{ color: 'black', marginRight: '8px' }} />
-            <span>discord_username</span>
+            <span>@ice_jake</span>
           </li>
         </ul>
       </div>
