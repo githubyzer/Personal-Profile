8c17bc36c694c57c72cee6014fe3a951f5570a03
Yzer Jake Navarroza <yzerjakenavarroza@gmail.com>
2024-10-17 08:17:34 +0800

    Added additional information about myself and tested clickable links

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,11 +3,27 @@ import React from 'react';
 const About = () => {
   return (
     <div className="mt-5">
-      <h1><b>About Me</b></h1>
+      <h1><b>About Yzer</b></h1>
       <p>
         I'm an 3rd Year Computer Science student from University of Cabuyao, 3CS-A with a passion for both programming and design. 
         My education background includes learning about algorithms, data structures, 
         and front-end development, as well as experience in freelance graphic design.
+      </p>
+      <h2><b>Random Facts About Yzer </b></h2>
+      <p>
+      <ul>
+        <li>I am an INTP-A. Imaginative and curious, people with the INTP personality type can find endless fascination in the workings of their own mind.</li>
+        <li>I am a dancer and a proud member of <b>Indak Dangal Dance Company</b>.</li>
+        <li>I have two cats named Sisa and Tambay.</li>
+        <li>I am born on May 19, 2004 in Carmona, Cavite.</li>
+        <li>I love music and stan multiple groups. 
+        <a href="https://www.instagram.com/blackpinkofficial">
+        <div>BLACKPINK</div></a>,
+        <a href="https://www.instagram.com/yg_treasure_official">
+        <div>TREASURE</div></a>, and 
+        <a href="https://www.instagram.com/meovv">
+        <div>MEOVV</div></a> are my ultimates.</li>
+      </ul>
       </p>
     </div>
   );
